3a60cb2a0be5ed8a13f46a0da2d615dcf1666d11
KgomotsoWelcome <47862122+KgomotsoWelcome@users.noreply.github.com>
2019-03-05 10:11:51 +0200

    Basic Interface

--- Client.java
+++ Client.java
@@ -1,70 +1,189 @@
-import java.net.*;
-import java.util.*;
 import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Client{
-	
-	public static void main(String []args) throws UnknownHostException, IOException {
-		
-		String machineName = "localhost";
-		int portNumber = 2015;
-		//DataInputStream input = null;
-		//DataOutputStream output = null;
-		Socket client = null; 
-		Scanner scan = new Scanner(System.in);
-		String line;
-		
-		try{
-			client = new Socket(machineName, portNumber);
-		}catch(IOException e){
-			System.out.println(e);
-		}
-		
-			DataInputStream input = new DataInputStream (client.getInputStream());
-			DataOutputStream output = new DataOutputStream(client.getOutputStream());
+public class Client {
 
+    private static int port = 12001;
+    static String name;
+    static String password;
+    static String client_recipient;
+    static Server server_class = new Server();
+    
+    /**This method implementation is used to clear the screen
+     * to keep the display of the application nice and neat
+     **/
+    public static void clearScreen() {  
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();  
+	} 
+    
+    public static void main(String[] args)
+    {
+        Socket client;
+        Scanner keyboard = new Scanner(System.in);
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            client = new Socket(host.getHostAddress(), port);
+
+            DataOutputStream output = new DataOutputStream(client.getOutputStream());
+            DataInputStream input = new DataInputStream(client.getInputStream());
+			
+			System.out.println("***************************Welcome to the ChatApp**********************");
+            
+            while(true){
+				
+				System.out.println("Please enter your name: ");
+				name = keyboard.nextLine();
+            
+				System.out.println("Please enter your password: ");
+				password = keyboard.nextLine();
+			
+				if((name.equals("wlckgo001"))||(name.equals("chnleo007"))||(name.equals("xkzmus001")))
+				{
+					System.out.println("You have been successfully connected!");
+					break;
+				}else{
+					System.out.println("The Username or Password you entered is incorrect. Please try again.");
+				}
+			}
+			
+			clearScreen(); // clears the screen
+            boolean nameReceived = false; //To check whether the server has received the name
+            while (!nameReceived) {
+                output.writeUTF(name); //Telling the server what is the online name of the client
+                if (input.readUTF().equals("name-confirmed")) //Confirmation from the server that the client name has been received
+                    nameReceived = true;
+            }
+
+            Thread receiver = createReceiveThread(input);
+            Thread sender = createSendThread(output, keyboard);
+
+			/**Prints out the names of the people that are online
+			 * to allow the user to choose who to communicate with
+			 **/
+			 
+			//int size = server_class.handler.size(); // getting the number of the clients connected
+            //System.out.println("The following people are online: ");
+            //System.out.println(size);
+            //for (int i = 0; i < size ; i++){
+				//System.out.println(server_class.handler.get(i));
+				//}
 		
-		Thread MessageToSend = new Thread(new Runnable()  
-        { 
+		System.out.println("Choose the person you would like to talk to.");
+		client_recipient = keyboard.nextLine();	
+            receiver.start();
+            sender.start();
+
+        } catch(UnknownHostException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Thread createReceiveThread(DataInputStream input)
+    {
+        Thread receiver = new Thread(new Runnable() {
             @Override
-            public void run() { 
-                while (true) { 
-  
-                    // read the message to deliver. 
-                    String message = scan.nextLine(); 
-                      
-                    try { 
-                        // write on the output stream 
-                        output.writeUTF(message); 
-                    } catch (IOException e) { 
-                        e.printStackTrace(); 
-                    } 
-                } 
-            } 
+            public void run() {
+                while(true)
+                {
+                    try {
+                        String info = input.readUTF();
+                        System.out.println(info);
+                        if (info.equals("receive-file")) {
+                                saveFile(input);
+                        }
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
         });
-        
-        Thread messageToRead = new Thread(new Runnable()  
-        { 
+
+        return receiver;
+    }
+
+    public static Thread createSendThread(DataOutputStream output, Scanner keyboard)
+    {
+        Thread sender = new Thread(new Runnable() {
             @Override
-            public void run() { 
-  
-                while (true) { 
-                    try { 
-                        // read the message sent to this client 
-                        String message = input.readUTF(); 
-                        System.out.println(message); 
-                    } catch (IOException e) { 
-  
-                        e.printStackTrace(); 
-                    } 
-                } 
-            } 
-        }); 
+            public void run() {
+				clearScreen();
+				System.out.println(name+ " --------> "+ client_recipient);
+				System.out.println("Type the message you want to send or type \"send-file\" to send a file.");
+				System.out.println("logout");
+                while(true)
+                {
+                    try {
+                        String info = keyboard.nextLine();
+                        output.writeUTF(name +" : " +info);
+                        output.flush();
+                        if (info.equals("send-file")) {
+                            sendFile(output, keyboard);
+                            System.out.println("File has been sent.");
+                        }
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        return sender;
+    }
+
+    static void sendFile(DataOutputStream output, Scanner keyboard) throws IOException
+    {
+        System.out.println("Type the name of the file: ");
+        String filename = keyboard.nextLine();
+
+        //Open the file and read from it, use File to get the size of the file, and use FileInputStream to read the data of the file
+        File file = new File(filename);
+        FileInputStream fis = new FileInputStream(file);
         
-        MessageToSend.start(); 
-        messageToRead.start();
+
+        byte[] buffer = new byte[8192]; //Create a buffer for the data to be transported
+        long length = file.length();
+
+        output.writeUTF(filename);
+        output.writeUTF(client_recipient);
+        output.writeLong(length);
+        output.flush(); //To make sure the stream is clean
+
+        int count; //To count how many bytes have been read every time
+        while ((count = fis.read(buffer)) > 0)
+            output.write(buffer, 0, count);
+
+        fis.close();
+    }
+
+    static void saveFile(DataInputStream input) throws IOException
+    {
+        String filename = name + input.readUTF();
+        long length = input.readLong();
+        int read;
+        long remaining = length;
+        byte[] buffer = new byte[8192];
+
+		System.out.println("Do you want to download the file? ");
+		System.out.println("1 - Yes I want to downlaod the file ");
+		System.out.println("No - I do not want to download the file ");
 		
-	}
+        FileOutputStream fos = new FileOutputStream(filename);
+
+        while((read = input.read(buffer, 0, Math.min(buffer.length, (int)remaining))) > 0) {
+            remaining -= read;
+            fos.write(buffer, 0, read);
+        }
+
+        System.out.println(filename+" has been saved.");
+        fos.close();
+
+    }
+
 }
-
-
--- Server.java
+++ Server.java
@@ -1,65 +1,152 @@
-import java.net.*;
 import java.io.*;
-import java.util.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Server{
-	
-	static ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
-	public static void main (String []args){
-		
-		int portNumber = 2015;
-		ServerSocket service = null;
-		Socket serviceSocket = null;
-		
-		
-		int clientCounter = 0; 
+public class Server
+{
 
-		
-		DataInputStream input;
-		DataOutputStream output;
-		String result;
+    private static int port = 12001;
+    static Client clientClass = new Client();
+    static ArrayList<ClientHandler> handler = new ArrayList<ClientHandler>();
+
+    public static void main(String[] args)
+    {
+		System.out.println("*******************************Server*********************************");
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
 			
-		try{
-		 service = new ServerSocket(portNumber);
-		 serviceSocket = new Socket();
-		
-		}catch(IOException e){
-			System.out.println(e);
-		}
-			
-		while(true){
-			try{
-				
-				System.out.println("Client is requesting to connect.....");
-				serviceSocket = service.accept();
-				System.out.println("Client request is accepted");
-				
-				input = new DataInputStream (serviceSocket.getInputStream());
-				output = new DataOutputStream(serviceSocket.getOutputStream());
-				
-				ClientThread connectedClients = new ClientThread(serviceSocket,"client " + clientCounter, input, output);
-				Thread thread = new Thread(connectedClients);
-				System.out.println("Adding this client to active client list");
-				clients.add(connectedClients);
-				thread.start();
-				
-				
-				clientCounter++;
-			}
-				//result = input.readLine();
-				//System.out.println("Message: "+result);
-				//input.close();
-				//output.close();
-				//serviceSocket.close();
-				//service.close();
-				
-			catch(IOException ex){
-					System.out.println(ex);
-			}
-		}
-		}
+            while(true) {
+				System.out.println();
+                System.out.println("Waiting for request from clients...");
+                Socket client = server.accept();
+                System.out.println("Received request from clients, connection established.");
+
+                DataInputStream input = new DataInputStream(client.getInputStream());
+                DataOutputStream output = new DataOutputStream(client.getOutputStream());
+
+                String name = input.readUTF(); //Read in the client online name
+                output.writeUTF("name-confirmed"); //Telling the client that the online name has been received
+
+                ClientHandler ch = new ClientHandler(client, new DataInputStream(client.getInputStream()), output, name);
+                handler.add(ch);
+                System.out.println(name+" has been added to the client list!");
+                ch.start();
+            }
+
+        } catch(IOException e) {
+            System.out.println("Sorry, could not open Data Streams.");
+        }
+    }
 }
 
+class ClientHandler extends Thread {
+    private Socket client;
+    private final DataInputStream input;
+    private final DataOutputStream output;
+    private boolean isOnline = true;
+    private final String name;
 
+    public ClientHandler(Socket client, DataInputStream input, DataOutputStream output, String name) {
+        this.client = client;
+        this.input = input;
+        this.output = output;
+        this.name = name;
+    }
 
+    public void run()
+    {
+        while(true) {
+            try {
+                String message = input.readUTF();
 
+                if (message.equals("logout")) {
+					System.out.println("Your friend has logged out.");
+                    isOnline = false;
+                    break;
+                }
+
+                if (message.equals("send-file")) {
+                    //Receive and save the file from source client
+                    output.writeUTF("send-file-confirmed");
+                    String filename = "server" + input.readUTF();
+                    String recipient = input.readUTF();
+                    saveFile(filename);
+
+                    //Find and send the file to the recipient
+                    for (ClientHandler ch : Server.handler) {
+                        if (ch.name.equals(recipient)) {
+                            ch.output.writeUTF("receive-file");
+                            output.flush();
+                            sendFile(ch.output, filename);
+                        }
+                    }
+                }
+                else {
+                    //Trying to get the client name that the message should goes to
+                    StringTokenizer messageHandler = new StringTokenizer(message, "@");
+                    String message1 = messageHandler.nextToken();
+                    String recipient = messageHandler.nextToken();
+
+                    //Looping through all the clients in the ArrayList to find the client that match the name
+                    for (ClientHandler ch : Server.handler) {
+                        if (ch.name.equals(recipient))
+                            ch.output.writeUTF(message1);
+                    }
+                }
+            } catch(IOException e) {
+                System.out.println("The client has disconnected.");
+                System.exit(0);
+            }
+        }
+
+        //Try to close all the stream
+        try {
+            client.close();
+            input.close();
+            output.close();
+        } catch(IOException e) {
+            System.out.println("All the open streams have been closed.");
+        }
+    }
+
+    void saveFile(String filename) throws IOException
+    {
+        long length = input.readLong(); //Get the size of the file
+        int read = 0; //To show how many bytes have been read each time
+        long remaining = length; //Remaining bytes to be read
+        byte[] buffer = new byte[8192];
+
+        FileOutputStream fos = new FileOutputStream(filename);
+        while ((read = input.read(buffer, 0, Math.min(buffer.length, (int)remaining))) > 0)  {
+            remaining -= read;
+            fos.write(buffer, 0, read);
+        }
+
+        System.out.println("File has been saved.");
+
+        fos.close();
+    }
+
+    void sendFile(DataOutputStream output, String filename) throws IOException
+    {
+        File file = new File(filename);
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] buffer = new byte[8192];
+        long length = file.length();
+
+        output.writeUTF(filename);
+        output.writeLong(length);
+        output.flush();
+
+        int count;
+        while ((count = fis.read(buffer)) > 0)
+            output.write(buffer, 0, count);
+
+        System.out.println("File has been sent.");
+        fis.close();
+    }
+
+}
