ad5dbd9d424634249cb710f922e62824df076786
Unaticlay <45321975+Unaticlay@users.noreply.github.com>
2019-03-05 19:49:21 +0200

    Mostly done
    Still need to handle the error when the user type the wrong filename

--- Server.java
+++ Server.java
@@ -6,13 +6,15 @@ import java.util.StringTokenizer;
 public class Server
 {
 
-    private static int port = 2222;
-    static ArrayList<ClientHandler> handler = new ArrayList<ClientHandler>();
+    private static int port = 12001;
+    static ArrayList<ClientHandler> handler = new ArrayList<>();
+    static ArrayList<ClientInfo> clientList = new ArrayList<>();
 
     public static void main(String[] args)
     {
         ServerSocket server = null;
         try {
+            loadClientList();
             server = new ServerSocket(port);
 
             while(true) {
@@ -23,10 +25,7 @@ public class Server
                 DataInputStream input = new DataInputStream(client.getInputStream());
                 DataOutputStream output = new DataOutputStream(client.getOutputStream());
 
-                String name = input.readUTF(); //Read in the client online name
-                output.writeUTF("name-confirmed"); //Telling the client that the online name has been received
-
-                ClientHandler ch = new ClientHandler(client, new DataInputStream(client.getInputStream()), output, name);
+                ClientHandler ch = new ClientHandler(client, input, output);
                 handler.add(ch);
                 System.out.println("New client has been added to the client list.");
                 ch.start();
@@ -39,31 +38,69 @@ public class Server
 
     }
 
+    static String logInCheck(DataInputStream input, DataOutputStream output) throws IOException {
+        while (true) {
+            String name = input.readUTF(); //Read in the client ID
+            String password = input.readUTF();
+
+            for (ClientInfo info : clientList) {
+                if (info.checkExist(name) && info.checkPassword(password)) {
+                    output.writeUTF("info-confirm");
+                    System.out.println("Client " + name + " has logged in.");
+                    return name;
+                }
+            }
+
+            output.writeUTF("login-fail");
+        }
+    }
+
+    //Load the client list from the file
+    static void loadClientList() throws IOException {
+        File listFile = new File("ClientList.txt");
+        BufferedReader input = new BufferedReader(new FileReader(listFile));
+        String info;
+        while ((info =input.readLine()) != null) {
+            StringTokenizer token = new StringTokenizer(info);
+            ClientInfo tempClient = new ClientInfo(token.nextToken(), token.nextToken());
+            clientList.add(tempClient);
+        }
+
+        input.close();
+    }
+
+
 }
 
 class ClientHandler extends Thread {
     private Socket client;
     private final DataInputStream input;
     private final DataOutputStream output;
-    private boolean isOnline = true;
-    private final String name;
+    private boolean isOnline = false;
+    private String name;
 
-    public ClientHandler(Socket client, DataInputStream input, DataOutputStream output, String name) {
+    public ClientHandler(Socket client, DataInputStream input, DataOutputStream output) {
         this.client = client;
         this.input = input;
         this.output = output;
-        this.name = name;
     }
 
     public void run()
     {
+        try {
+            name = Server.logInCheck(input, output);
+            isOnline = true;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
         while(true) {
             try {
                 String message = input.readUTF();
 
                 if (message.equals("logout")) {
+                    output.writeUTF("logout");
                     isOnline = false;
-                    break;
+                    break; //It will break out of this loop and go to the try-catch block
                 }
 
                 if (message.equals("send-file")) {
@@ -76,41 +113,67 @@ class ClientHandler extends Thread {
                     //Find and send the file to the recipient
                     for (ClientHandler ch : Server.handler) {
                         if (ch.name.equals(recipient)) {
-                            ch.output.writeUTF("receive-file");
+                            ch.output.writeUTF("receive-file-confirmation");
+                            ch.output.writeUTF(filename);
+                            ch.output.writeUTF(name);
                             output.flush();
-                            sendFile(ch.output, filename);
                         }
                     }
 
-
+                }
+                //Handling for file transmission, Y stands for agree to accept the file, vice versa
+                else if (message.equals("Y")) {
+                    String filename = input.readUTF();
+                    output.writeUTF("receive-file");
+                    sendFile(output, filename);
+                }
+                else if (message.equals("N")) { //Handling for client that refused to accept the file
+                    String fileSender = input.readUTF();
+                    for (ClientHandler ch : Server.handler) {
+                        if (ch.name.equals(fileSender)) {
+                            ch.output.writeUTF(name + " refused to receive your file.");
+                            output.flush();
+                        }
+                    }
                 }
                 else {
                     //Trying to get the client name that the message should goes to
                     StringTokenizer messageHandler = new StringTokenizer(message, "@");
-                    messageHandler.nextToken();
+                    String messageBody = messageHandler.nextToken();
                     String recipient = messageHandler.nextToken();
+                    messageBody = name + ": " + messageBody;
+                    boolean online = false;
 
                     //Looping through all the clients in the ArrayList to find the client that match the name
                     for (ClientHandler ch : Server.handler) {
-                        if (ch.name.equals(recipient))
-                            ch.output.writeUTF(message);
+                        if (ch.isOnline && ch.name.equals(recipient)) { //isOnline makes sure it is a logged in client instead of client that are still trying to log in
+                            ch.output.writeUTF(messageBody);
+                            online = true;
+                            //break;
+                        }
                     }
+
+                    if (!online)
+                        output.writeUTF("User " + recipient + " is not online.");
                 }
             } catch(IOException e) {
                 e.printStackTrace();
-                System.exit(0);
             }
         }
 
-        //Try to close all the stream
+        //Try to close all the stream and remove itself from the handler ArrayList
         try {
-            client.close();
             input.close();
             output.close();
+            client.close();
+            System.out.println("Client " + name + " has gone offline");
+            Server.handler.remove(this);
+            return; //To terminate the thread
         } catch(IOException e) {
             System.out.println(e);
         }
     }
+
 
     void saveFile(String filename) throws IOException
     {
@@ -150,4 +213,29 @@ class ClientHandler extends Thread {
         fis.close();
     }
 
+}
+
+class ClientInfo {
+    private String id;
+    private String password;
+    public ClientInfo(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkExist(String name) {
+        return id.equals(name);
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
 }
